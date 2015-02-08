5d0bc980594dba8952016a1b0960eaafe1389d17
Dustin Sallings <dustin@spy.net>
2015-02-07 23:24:59 -0800

    Removed all the TODO types
    Too confusing.  Better to add them as needed.

--- nmea_types.go
+++ nmea_types.go
@@ -2,76 +2,6 @@ package nmea
 
 import "time"
 
-// AAM represents a Waypoint Arrival Alarm message.
-type AAM struct {
-	// TODO
-}
-
-// A AAMHandler handles AAM messages from a stream.
-type AAMHandler interface {
-	HandleAAM(AAM)
-}
-
-// ALM represents a Almanac data message.
-type ALM struct {
-	// TODO
-}
-
-// A ALMHandler handles ALM messages from a stream.
-type ALMHandler interface {
-	HandleALM(ALM)
-}
-
-// APA represents a Auto Pilot A sentence message.
-type APA struct {
-	// TODO
-}
-
-// A APAHandler handles APA messages from a stream.
-type APAHandler interface {
-	HandleAPA(APA)
-}
-
-// APB represents a Auto Pilot B sentence message.
-type APB struct {
-	// TODO
-}
-
-// A APBHandler handles APB messages from a stream.
-type APBHandler interface {
-	HandleAPB(APB)
-}
-
-// BOD represents a Bearing Origin to Destination message.
-type BOD struct {
-	// TODO
-}
-
-// A BODHandler handles BOD messages from a stream.
-type BODHandler interface {
-	HandleBOD(BOD)
-}
-
-// BWC represents a Bearing using Great Circle route message.
-type BWC struct {
-	// TODO
-}
-
-// A BWCHandler handles BWC messages from a stream.
-type BWCHandler interface {
-	HandleBWC(BWC)
-}
-
-// DTM represents a Datum being used. message.
-type DTM struct {
-	// TODO
-}
-
-// A DTMHandler handles DTM messages from a stream.
-type DTMHandler interface {
-	HandleDTM(DTM)
-}
-
 // FixQuality represents the quality of a position fix in a GGA packet.
 type FixQuality int
 
@@ -131,16 +61,6 @@ type GLLHandler interface {
 	HandleGLL(GLL)
 }
 
-// GRS represents a GPS Range Residuals message.
-type GRS struct {
-	// TODO
-}
-
-// A GRSHandler handles GRS messages from a stream.
-type GRSHandler interface {
-	HandleGRS(GRS)
-}
-
 type GSAFix int
 
 const (
@@ -167,16 +87,6 @@ type GSAHandler interface {
 	HandleGSA(GSA)
 }
 
-// GST represents a GPS Pseudorange Noise Statistics message.
-type GST struct {
-	// TODO
-}
-
-// A GSTHandler handles GST messages from a stream.
-type GSTHandler interface {
-	HandleGST(GST)
-}
-
 type GSVSatInfo struct {
 	PRN       int
 	Elevation int
@@ -197,44 +107,6 @@ type GSVHandler interface {
 	HandleGSV(GSV)
 }
 
-// MSK represents a send control for a beacon receiver message.
-type MSK struct {
-	// TODO
-}
-
-// A MSKHandler handles MSK messages from a stream.
-type MSKHandler interface {
-	HandleMSK(MSK)
-}
-
-// MSS represents a Beacon receiver status information message.
-type MSS struct {
-}
-
-// A MSSHandler handles MSS messages from a stream.
-type MSSHandler interface {
-	HandleMSS(MSS)
-}
-
-// RMA represents a recommended Loran data message.
-type RMA struct {
-}
-
-// A RMAHandler handles RMA messages from a stream.
-type RMAHandler interface {
-	HandleRMA(RMA)
-}
-
-// RMB represents a recommended navigation data for gps message.
-type RMB struct {
-	// TODO
-}
-
-// A RMBHandler handles RMB messages from a stream.
-type RMBHandler interface {
-	HandleRMB(RMB)
-}
-
 // RMC represents a recommended minimum data for gps message.
 type RMC struct {
 	Timestamp           time.Time
@@ -250,46 +122,6 @@ type RMCHandler interface {
 	HandleRMC(RMC)
 }
 
-// RTE represents a route message message.
-type RTE struct {
-	// TODO
-}
-
-// A RTEHandler handles RTE messages from a stream.
-type RTEHandler interface {
-	HandleRTE(RTE)
-}
-
-// TRF represents a Transit Fix Data message.
-type TRF struct {
-	// TODO
-}
-
-// A TRFHandler handles TRF messages from a stream.
-type TRFHandler interface {
-	HandleTRF(TRF)
-}
-
-// STN represents a Multiple Data ID message.
-type STN struct {
-	// TODO
-}
-
-// A STNHandler handles STN messages from a stream.
-type STNHandler interface {
-	HandleSTN(STN)
-}
-
-// VBW represents a dual Ground / Water Spped message.
-type VBW struct {
-	// TODO
-}
-
-// A VBWHandler handles VBW messages from a stream.
-type VBWHandler interface {
-	HandleVBW(VBW)
-}
-
 // VTG represents a Vector track an Speed over the Ground message.
 type VTG struct {
 	True, Magnetic float64
@@ -299,56 +131,6 @@ type VTG struct {
 // A VTGHandler handles VTG messages from a stream.
 type VTGHandler interface {
 	HandleVTG(VTG)
-}
-
-// WCV represents a Waypoint closure velocity (Velocity Made Good) message.
-type WCV struct {
-	// TODO
-}
-
-// A WCVHandler handles WCV messages from a stream.
-type WCVHandler interface {
-	HandleWCV(WCV)
-}
-
-// WPL represents a Waypoint Location information message.
-type WPL struct {
-	// TODO
-}
-
-// A WPLHandler handles WPL messages from a stream.
-type WPLHandler interface {
-	HandleWPL(WPL)
-}
-
-// XTC represents a cross track error message.
-type XTC struct {
-	// TODO
-}
-
-// A XTCHandler handles XTC messages from a stream.
-type XTCHandler interface {
-	HandleXTC(XTC)
-}
-
-// XTE represents a measured cross track error message.
-type XTE struct {
-	// TODO
-}
-
-// A XTEHandler handles XTE messages from a stream.
-type XTEHandler interface {
-	HandleXTE(XTE)
-}
-
-// ZTG represents a Zulu (UTC) time and time to go (to destination) message.
-type ZTG struct {
-	// TODO
-}
-
-// A ZTGHandler handles ZTG messages from a stream.
-type ZTGHandler interface {
-	HandleZTG(ZTG)
 }
 
 // ZDA represents a Date and Time message.
