a4c8427a4a30dd6183b4545fad32dc7cbffaba9a
Dustin Sallings <dustin@spy.net>
2015-02-09 21:59:11 -0800

    Document the starting point a bit better

--- doc.go
+++ doc.go
@@ -1,4 +1,8 @@
 /*
  Package NMEA provides functionality for understanding NMEA streams from a GPS.
+
+In its most simple usage, you define a type that satisfies the
+"Handler" types for the various types of messages you wish to receive,
+and pass it to Process.
 */
 package nmea
