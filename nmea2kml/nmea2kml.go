4ee80d401d137acdeceb69903c7561c67164ea05
Dustin Sallings <dustin@spy.net>
2016-11-21 08:42:55 -0800

    Declare proper namespace for gx: extensions

--- nmea2kml/nmea2kml.go
+++ nmea2kml/nmea2kml.go
@@ -11,7 +11,9 @@ import (
 )
 
 const kmlHeader = `<?xml version="1.0" encoding="UTF-8"?>
-<kml xmlns="http://www.opengis.net/kml/2.2">
+<kml xmlns="http://www.opengis.net/kml/2.2"
+          xmlns:gx="http://www.google.com/kml/ext/2.2">
+
 <Folder>`
 const kmlPoint = `<Placemark>
   <TimeStamp>{{.TS}}</TimeStamp>
